0ea5d56c1ec27e4a2d37c7eb13abedf673e33cd8
Cassidy <cassidy.pignatello@gmail.com>
2020-01-25 16:15:36 +0700

    initial commit

---
+++ assets/php/footer.php
@@ -0,0 +1,63 @@
+ <?php 
+
+ echo "<!-- Footer -->
+      <footer class='footer py-7'>
+        <div class='container'>
+          <div class='row gap-y'>
+            <div class='col-6 col-md-3 col-xl-2'>
+              <h6 class='mb-4 mt-1'><strong>DOCS</strong></h6>
+              <div class='nav flex-column'>
+                <a class='nav-link' href='https://dev.bitshares.works/en/master/' target='_blank'>Dev Integration Guide</a>
+                <a class='nav-link' href='https://docs.bitshares.org/en/master/' target='_blank'>Documentation</a>
+                <a class='nav-link' href='https://doxygen.bitshares.org' target='_blank'>Doxygen</a>
+                <a class='nav-link' href='https://github.com/bitshares-foundation/bitshares.foundation/blob/master/download/articles/BitSharesBlockchain.pdf' target='_blank'>Revised Whitepaper</a>
+                
+              </div>
+            </div>
+
+            <div class='col-6 col-md-3 col-xl-2'>
+              <h6 class='mb-4 mt-1'><strong>ACCESS</strong></h6>
+              <div class='nav flex-column'>
+                <a class='nav-link' href='https://wallet.bitshares.org' target='_blank'>BitShares UI</a>
+                <a class='nav-link' href='https://bitshares-explorer.io/#/dashboard' target='_blank'>Block Explorer</a>
+                <a class='nav-link' href='register.html'>Create Free Account</a>
+                <a class='nav-link' href='../assets/bitshares-media-kit.zip'>Download Media Kit</a>
+              </div>
+            </div>
+
+            <div class='col-6 col-md-6 col-xl-2'>
+              <h6 class='mb-4 mt-1'><strong>COMMUNITY</strong></h6>
+              <div class='nav flex-column'>
+                <a class='nav-link' href='https://bitsharestalk.org' target='_blank'>Forums</a>
+                <a class='nav-link' href='https://github.com/bitshares' target='_blank'>GitHub</a>
+                <a class='nav-link' href='https://hackthedex.io' target='_blank'>HackTheDex Bounty</a>
+              </div>
+            </div>
+              
+              <div class='col-6 col-md-6 col-xl-2'>
+              <h6 class='mb-4 mt-1'><strong>BITSHARES.ORG</strong></h6>
+              <div class='nav flex-column'>
+                <a class='nav-link' href='contact.html'>Contact</a>
+              </div>
+            </div>
+
+            <div class='col-6 col-md-6 col-xl-2 offset-md-2 text-center'>
+                <h6 class='mb-4 mt-1'><strong>OUR&nbsp;NEWSLETTER</strong></h6>
+                <div class='flex-shrink'>  
+                <a class='btn btn-block btn-primary' href='https://bitsharestalk.org'>Subscribe</a>
+                </div>
+                <br/>
+                <div class='social social-bordered'>
+                <a class='social-facebook' href='#'><i class='fa fa-facebook'></i></a>
+                <a class='social-twitter' href='#'><i class='fa fa-twitter'></i></a>
+                <a class='social-youtube' href='#'><i class='fa fa-youtube'></i></a>
+                <a class='social-instagram' href='#'><i class='fa fa-instagram'></i></a>
+              </div>
+            </div>
+          </div>
+            
+            
+        </div>
+      </footer><!-- /.footer --> ";
+      
+?>
---
+++ assets/php/header.php
@@ -0,0 +1,84 @@
+<?php
+
+   echo"
+   <!-- Navbar -->
+   <nav class='navbar navbar-expand-lg navbar-light navbar-stick-dark' data-navbar='sticky'>
+      <div class='container'>
+
+        <div class='navbar-left'>
+          <button class='navbar-toggler' type='button'>&#9776;</button>
+          <a class='navbar-brand' href='../index.html'>
+            <img class='logo-dark' src='assets/images/blue-logo.png' alt='logo'>
+            <img class='logo-light' src='assets/images/light-logo.png' alt='logo'>
+          </a>
+        </div>
+
+          <div class='navbar-right'>
+        <section class='navbar-mobile '>
+          <span class='navbar-divider d-mobile-none'></span>
+
+          <ul class='nav nav-navbar'>
+            <li class='nav-item'>
+              <a class='nav-link' href='index.html'>Home</a>
+            </li>
+            <li class='nav-item'>
+              <a class='nav-link active' href='#'>DOCS <span class='arrow'></span></a>
+              <ul class='nav'>
+                <li class='nav-item'>
+                  <a class='nav-link' href='https://docs.bitshares.org/en/master/' target='_blank'>BitShares Docs</a>
+                </li>
+
+                <li class='nav-item'>
+                  <a class='nav-link' href='https://dev.bitshares.works/en/master/' target='_blank'>Integration for Developers</a>
+                </li>
+
+                <li class='nav-item'>
+                  <a class='nav-link' href='https://doxygen.bitshares.org' target='_blank'>Doxygen</a>
+                </li>
+
+                <li class='nav-item'>
+                  <a class='nav-link' href='https://github.com/bitshares-foundation/bitshares.foundation/blob/master/download/articles/BitSharesBlockchain.pdf' target='_blank'>Revised Whitepaper</a>
+                </li>
+              </ul>
+            </li>
+
+            <li class='nav-item'>
+              <a class='nav-link' href='https://news.bitshares.org' target='_blank'>News</a>
+            </li>
+
+            <li class='nav-item'>
+              <a class='nav-link' href='#'>TECHNOLOGY <span class='arrow'></span></a>
+              <nav class='nav'>
+                <a class='nav-link' href='technology/decentralized-asset-exchange.html'>Decentralized Asset Exchange</a>
+                <a class='nav-link' href='technology/delegated-proof-of-stake-consensus.html'>DPoS Consensus</a>
+                <a class='nav-link' href='technology/dynamic-account-permissions.html'>Dynamic Account Permissions</a>
+                <a class='nav-link' href='technology/named-accounts.html'>Named Accounts</a>
+                  <a class='nav-link' href='technology/industrial-performance-scalability.html'>Performance and Scalability</a>
+                  <a class='nav-link' href='technology/price-stable-cryptocurrencies.html'>Price-Stable-Cryptocurrencies</a>
+                  <a class='nav-link' href='technology/recurring-scheduled-payments.html'>Recurring & Scheduled Payments</a>
+                  <a class='nav-link' href='technology/referral-rewards-program.html'>Referral Rewards Program</a>
+                  <a class='nav-link' href='technology/stakeholder-approved-funding.html'>Stakeholder Approved Funding</a>
+                  <a class='nav-link' href='technology/user-issued-assets.html'>User-Issued Assets</a>
+              </nav>
+            </li>
+
+              <li class='nav-item nav-mega'>
+              <a class='nav-link' href='https://wallet.bitshares.org' target='_blank'>BITSHARES UI</a>
+            </li>
+
+            <li class='nav-item nav-mega'>
+              <a class='nav-link' href='download.html'>DOWNLOAD</a>
+            </li>
+
+              <li class='nav-item nav-mega'>
+              <a class='nav-link' href='contact.html'>CONTACT</a>
+            </li>
+
+          </ul>
+        </section>
+          </div>
+
+      </div>
+    </nav><!-- /.navbar --> ";
+
+?>
